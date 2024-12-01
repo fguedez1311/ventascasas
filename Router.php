89c8347ae0ee68f8e11922c8bb89e69e700570e7
“fguedez1311” <fguedez1311@gmail.com>
2024-12-01 07:07:09 -0400

    Registrando URL's y los metodos asociados a ellas

--- Router.php
+++ Router.php
@@ -2,8 +2,26 @@
     namespace MVC;
 
     class Router{
-        
-        
+        public $rutasGET=[];
+        public $utasPOST=[];
+        public function get($url,$fn){
+            $this->rutasGET[$url]=$fn;
+        }
+        public function comprobarRutas(){
+            // Leemos la url actual
+            $urlActual=$_SERVER['PATH_INFO'] ?? '/';
+
+            // El tipo de método GET o POST
+            $metodo=$_SERVER['REQUEST_METHOD'];
+            if ($metodo==='GET'){
+                $fn=$this->rutasGET[$urlActual] ?? null;
+            }
             
-        
+            if ($fn){
+                call_user_func($fn,$this);
+            }
+            else{
+                echo "Página no encontrada";
+            }
+        }
     }
