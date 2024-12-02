5775c4df98817092651680fd85fcda38d24bac17
“fguedez1311” <fguedez1311@gmail.com>
2024-12-02 08:51:10 -0400

    Consultando el Modelo en el Controlador y pasar los resultado hacia vista

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -2,12 +2,15 @@
 
     namespace Controllers;
     use MVC\Router;
+    use Model\Propiedad;
 
     class PropiedadController{
         public static function index(Router $router){
-            
+            $propiedades=Propiedad::all();
+            $resultado=null;
             $router->render('propiedades/admin',[
-                
+                'propiedades'=>$propiedades,
+                'resultado'=>$resultado
             ]);
         }
         public static function crear(){
