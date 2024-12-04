7ce2c591687fe201a3caff66d7a4fa208a95bfb3
“fguedez1311” <fguedez1311@gmail.com>
2024-12-04 15:11:46 -0400

    Creando el controlador para vendedores

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
 
     use MVC\Router;
     use Controllers\PropiedadController;
+    use Controllers\VendedorController;
 
     
     $router=new Router();
@@ -13,6 +14,12 @@
     $router->get('/propiedades/actualizar',[PropiedadController::class,'actualizar']);
     $router->post('/propiedades/actualizar',[PropiedadController::class,'actualizar']);
     $router->post('/propiedades/eliminar',[PropiedadController::class,'eliminar']);
+
+    $router->get('/vendedores/crear',[VendedorController::class,'crear']);
+    $router->post('/vendedores/crear',[VendedorController::class,'crear']);
+    $router->get('/vendedores/actualizar',[VendedorController::class,'actualizar']);
+    $router->post('/vendedores/actualizar',[VendedorController::class,'actualizar']);
+    $router->post('/vendedores/eliminar',[VendedorController::class,'eliminar']);
     
     $router->comprobarRutas();
     
