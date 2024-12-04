8e5ca90b6f3c63448e6d58a090d7e828f4c6912d
“fguedez1311” <fguedez1311@gmail.com>
2024-12-04 19:33:23 -0400

    Actualizamos Vendedores

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -28,8 +28,32 @@
                 'vendedor'=>$vendedor
             ]);
         }
-        public static function actualizar(){
-            echo "Actualizar Vendedor";
+        public static function actualizar(Router $router){
+            $errores=Vendedor::getErrores();
+
+            $id=validaORedireccionar('/admin');
+
+            $vendedor=Vendedor::find($id);
+
+            if ($_SERVER['REQUEST_METHOD']==='POST'){
+                //Asignar los valores
+                $args=$_POST['vendedor'];
+        
+                // Sincronizar objeto en memoria con lo que el usuario escribió
+                $vendedor->sincronizar($args);
+                
+                //Validación
+                $errores=$vendedor->validar();
+                if (empty($errores)){
+                    $vendedor->guardar();
+                }
+        
+            }
+
+            $router->render('vendedores/actualizar',[
+                'errores'=>$errores,
+                'vendedor'=>$vendedor
+            ]);
         }
         public static function eliminar(){
             echo "Eliminar Vendedor";
