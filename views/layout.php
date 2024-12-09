4e19e8304820c0ccae083971c43a81cba5f7ed56
“fguedez1311” <fguedez1311@gmail.com>
2024-12-08 20:11:44 -0400

    Creando las urls y sus páginas públicas

--- views/layout.php
+++ views/layout.php
@@ -30,12 +30,12 @@
                 <div class="derecha">
                     <img src="/build/img/dark-mode.svg" alt="" class="dark-mode-boton">
                     <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
+                        <a href="/nosotros">Nosotros</a>
+                        <a href="/propiedades">Anuncios</a>
+                        <a href="/blog">Blog</a>
+                        <a href="/contacto">Contacto</a>
                         <?php if($auth): ?>
-                            <a href="cerrar-session.php">Cerrar Sesión </a>  
+                            <a href="/cerrar-session">Cerrar Sesión </a>  
                         <?php endif; ?>
                     </nav>
                 </div>
---
+++ views/paginas/propiedades.php
@@ -0,0 +1,10 @@
+
+<main class="contenedor">
+        <section class="section contenedor">
+            <h2>Casa y Depas en venta</h2>
+            <?php
+                
+                include 'listado.php';
+           ?>
+            
+</main>
